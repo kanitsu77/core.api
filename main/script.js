e20480bfa6d75c92b25645c4e4c04fdb11bb60b0
Mahpud || Nixx <bamat7010@gmail.com>
2026-06-25 17:46:04 +0700

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://core-api-eta.vercel.app";
+const BASE_URL = "https://core-api.vercel.app";
 
 let dat = {};
 let activeApi = null, activeFilter = "ALL", keyShown = false, chatOpen = false;
