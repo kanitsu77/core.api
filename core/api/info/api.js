64a55a9082f5dece8827163b98f3e29bcc2cb327
Mahpud || Nixx <bamat7010@gmail.com>
2026-06-28 11:16:56 +0700

    Create api.js

---
+++ core/api/info/api.js
@@ -0,0 +1,37 @@
+const db = require("../../../main/database.json");
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+
+  try {
+    let totalEndpoint = 0;
+    const endpointList = {};
+    const categories = {};
+
+    for (const [category, endpoints] of Object.entries(db)) {
+      const names = Object.keys(endpoints);
+      totalEndpoint += names.length;
+      endpointList[category] = names;
+      categories[category] = names.length;
+    }
+
+    return res.json({
+      status: true,
+      creator: "Nixx",
+      url: `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}`,
+      endpoint: totalEndpoint,
+      categories,
+      endpointList,
+      result: db
+    });
+
+  } catch (e) {
+    return res.status(500).json({
+      status: false,
+      creator: "Nixx",
+      message: "Terjadi kesalahan",
+      error: e.message
+    });
+  }
+};
