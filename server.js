361ba9dbf66cd27a81558e77c85754a30226fd3c
Mahpud || Nixx <bamat7010@gmail.com>
2026-07-11 06:12:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "main")));
 app.use("/src", express.static(path.join(__dirname, "src")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(apiLogger)
 
 function wrapHandler(handler) {
